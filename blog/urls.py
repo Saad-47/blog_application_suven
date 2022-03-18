c3a203c6f4cf02c6d0eb972f9abe13503f7a8655
saad_shaikh <74404868+Saad-47@users.noreply.github.com>
2022-03-18 14:43:19 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 app_name	=	'blog'
 urlpatterns	=	[
-	path('', views.PostListView.as_view(), name='post_list'),
+	path("", views.PostListView.as_view(), name='post_list'),
 	path('<int:year>/<int:month>/<int:day>/<slug:post>/',
 									views.post_detail,
 									name='post_detail'),
