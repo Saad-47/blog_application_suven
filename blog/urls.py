1e0d7e83e4bc316b98af6c8866d3062035b1b4f5
Saad_Shaikh <mdsaadshaikh545@gmail.com>
2022-03-18 13:49:35 +0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import	path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+app_name	=	'blog'
+urlpatterns	=	[
+	path('', views.PostListView.as_view(), name='post_list'),
+	path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+									views.post_detail,
+									name='post_detail'),
+    path('<int:post_id>/share/',views.post_share,
+                                name='post_share'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
